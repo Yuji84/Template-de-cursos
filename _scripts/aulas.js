e43d7864fdefdb0bb12b44d254ad19b4d707a602
Eduardo Yuji Matheus <eymath@hotmail.com>
2020-04-15 19:01:45 -0300

    atual

---
+++ _scripts/aulas.js
@@ -0,0 +1,106 @@
+function names(){
+    var aula01 = "Aula 01 - Introdução"
+    var aula02 = "Aula 02"
+    var aula03 = "Aula 03"
+    var aula04 = "Aula 04"
+    var aula05 = "Aula 05"
+    var aula06 = "Aula 06"
+    var aula07 = "Aula 07"
+    var aula08 = "Aula 08"
+    var aula09 = "Aula 09"
+    var aula10 = "Aula 10"
+    var aula11 = "Aula 11"
+    var aula12 = "Aula 12"
+    var aula13 = "Aula 13"
+    var aula14 = "Aula 14"
+    var aula15 = "Aula 15"
+    var aula16 = "Aula 16"
+    var aula17 = "Aula 17"
+    var aula18 = "Aula 18"
+    var aula19 = "Aula 19"
+    var aula20 = "Aula 20"
+
+    var a1=document.getElementsByClassName("aula01")
+    a1[0].innerHTML = aula01
+    a1[1].innerHTML = a1[0].innerHTML
+
+    var a2=document.getElementsByClassName("aula02")
+    a2[0].innerHTML = aula02
+    a2[1].innerHTML = a2[0].innerHTML
+
+    var a3=document.getElementsByClassName("aula03")
+    a3[0].innerHTML = aula03
+    a3[1].innerHTML = a3[0].innerHTML
+
+    var a4=document.getElementsByClassName("aula04")
+    a4[0].innerHTML = aula04
+    a4[1].innerHTML = a4[0].innerHTML
+
+    var a5=document.getElementsByClassName("aula05")
+    a5[0].innerHTML = aula05
+    a5[1].innerHTML = a5[0].innerHTML
+
+    var a6=document.getElementsByClassName("aula06")
+    a6[0].innerHTML = aula06
+    a6[1].innerHTML = a6[0].innerHTML
+
+    var a7=document.getElementsByClassName("aula07")
+    a7[0].innerHTML = aula07
+    a7[1].innerHTML = a7[0].innerHTML
+
+    var a8=document.getElementsByClassName("aula08")
+    a8[0].innerHTML = aula08
+    a8[1].innerHTML = a8[0].innerHTML
+
+    var a9=document.getElementsByClassName("aula09")
+    a9[0].innerHTML = aula09
+    a9[1].innerHTML = a9[0].innerHTML
+
+    var a10=document.getElementsByClassName("aula10")
+    a10[0].innerHTML = aula10
+    a10[1].innerHTML = a10[0].innerHTML
+
+    var a11=document.getElementsByClassName("aula11")
+    a11[0].innerHTML = aula11
+    a11[1].innerHTML = a11[0].innerHTML
+
+    var a12=document.getElementsByClassName("aula12")
+    a12[0].innerHTML = aula12
+    a12[1].innerHTML = a12[0].innerHTML
+
+    var a13=document.getElementsByClassName("aula13")
+    a13[0].innerHTML = aula13
+    a13[1].innerHTML = a13[0].innerHTML
+
+    var a14=document.getElementsByClassName("aula14")
+    a14[0].innerHTML = aula14
+    a14[1].innerHTML = a14[0].innerHTML
+
+    var a15=document.getElementsByClassName("aula15")
+    a15[0].innerHTML = aula15
+    a15[1].innerHTML = a15[0].innerHTML
+
+    var a16=document.getElementsByClassName("aula16")
+    a16[0].innerHTML = aula16
+    a16[1].innerHTML = a16[0].innerHTML
+
+    var a17=document.getElementsByClassName("aula17")
+    a17[0].innerHTML = aula17
+    a17[1].innerHTML = a17[0].innerHTML
+
+    var a18=document.getElementsByClassName("aula18")
+    a18[0].innerHTML = aula18
+    a18[1].innerHTML = a18[0].innerHTML
+
+    var a19=document.getElementsByClassName("aula19")
+    a19[0].innerHTML = aula19
+    a19[1].innerHTML = a19[0].innerHTML
+
+    var a20=document.getElementsByClassName("aula20")
+    a20[0].innerHTML = aula20
+    a20[1].innerHTML = a20[0].innerHTML
+
+    var a1=document.getElementsByClassName("aula01")
+    a1[0].innerHTML = aula01
+    a1[1].innerHTML = a1[0].innerHTML
+    }
